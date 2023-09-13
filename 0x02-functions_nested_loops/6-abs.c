c0925a311d5adf3b8c7e8c642e261f2ae8db68f1
Mohakad <mohkadtrd@gmail.com>
2023-09-13 08:38:47 -0700

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_abs - absolute num
+  *@num: absoulutr num
+  *Return: num or - num
+  */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return -num;
+	}
+	else
+	{
+		return num;
+	}
+}
