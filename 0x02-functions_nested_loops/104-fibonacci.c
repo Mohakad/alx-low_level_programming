e99859cbd1b7b50c15806bb4b6a4b33e72e0c540
Mohakad <mohkadtrd@gmail.com>
2023-09-14 09:18:03 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  *main- fibonachii fir 98
+  *Return: 0.
+  */
+int main(void)
+{
+	long int f, s, n;
+
+	f = 1;
+	s = 2;
+	n = f + s;
+	int coun = 3;
+
+	printf("%lu, ", f);
+	printf("%lu, ", s);
+	while (coun <= 98)
+	{
+		if (coun == 98)
+		{
+			printf("%lu\n", n);
+		}
+		else
+		{
+			printf("%lu, ", n);
+		}
+		f = s;
+		s = n;
+		n = f + s;
+		coun++;
+	}
+	return (0);
+}
