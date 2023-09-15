9cba5aa43992ad7c5474412bcb8612bf304fa24f
Mohakad <mohkadtrd@gmail.com>
2023-09-15 06:25:48 -0700

    ./6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - straight line
+
+ * @n: number of times _ should be printed
+
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
