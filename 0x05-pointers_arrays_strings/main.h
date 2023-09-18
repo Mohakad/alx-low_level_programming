73cd2812df9165320f7f9b81ba7a397a8ed2083e
Mohakad <mohkadtrd@gmail.com>
2023-09-18 05:32:40 -0700

    0 reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void swap_int(int *a, int *b);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
