fb82dde70fb8f9bbf1f74c47b8645b6829138d26
Mohakad <mohkadtrd@gmail.com>
2023-09-18 08:38:55 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half
+ * @str: input
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+	int a, h, lon;
+
+	lon = 0;
+	for (a = 0; str[a] != '\0'; a++)
+		lon++;
+	h = (lon / 2);
+	if ((lon % 2) == 1)
+		h = ((lon - 1) / 2);
+	for (a = h; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
