298b0ed2f8d9bc6f4152885dc569102309ce6c0e
Mohakad <mohkadtrd@gmail.com>
2023-09-20 09:14:14 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,19 +7,14 @@
   */
 char *_strcat(char *dest, char *src)
 {
-	int ind1, ind2;
-
-	ind1 = 0;
+	int ind1 = 0, ind2;
 
 	while (dest[ind1] != '\0')
 		ind1++;
-	ind2 = 0;
-	while (src[ind2] != '\0')
+	for (ind2 = 0; ind2 <= ind1 && src[ind2] != '\0'; ind2++)
 	{
-		dest[ind1] = src[ind2];
-		ind1++;
-		ind2++;
+		dest[ind2 + ind1] = src[ind2];
 	}
-	dest[ind1] = '\0';
+	dest[ind2 + ind1] = '\0';
 	return (dest);
 }
