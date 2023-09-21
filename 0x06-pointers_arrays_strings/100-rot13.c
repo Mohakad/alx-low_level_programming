341a17b659540b004da1f907b224b6be56e4d643
Mohakad <mohkadtrd@gmail.com>
2023-09-21 06:35:36 -0700

    ./100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *rot13 - encryter using rot13
+  *@s : string to enc
+  * Return: encrypted string
+  */
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
+			s[i] += 13;
+		else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+				s[i] -= 13;
+	}
+	return (s);
+}
