70082c11010f1574b7469d145c9b2d4b8809c0ab
Mohakad <mohkadtrd@gmail.com>
2023-09-21 05:33:27 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,8 @@ char *cap_string(char *str)
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||		    str[index - 1] == ';' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
 				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
 				str[index - 1] == '?' ||
