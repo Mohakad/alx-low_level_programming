b0f68b233b9b69e1ac519279c1ee06e6724bcc46
Mohakad <mohkadtrd@gmail.com>
2023-09-25 05:05:41 -0700

    ./0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
   *_memset - set memory with conta byte
-  *@s: source 
-  *@:b bytes to fill
+  *@s: source
+  *@b: bytes to fill
   *@n: n timees
-  *Return : s 
-  */
+  * Return: return pointer s.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
-	while (i < n)
+
+	while (n > 0)
 	{
-		s[i] = b[i];
+		s[i] = b;
 		i++;
+		n--;
 	}
 	return (s);
 }
