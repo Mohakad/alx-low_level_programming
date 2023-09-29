b2fe303323c2e9f0a554523a1b0f9ded278b6824
Mohakad <mohkadtrd@gmail.com>
2023-09-29 06:30:11 -0700

    ./0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_puts_recursion - print str
+  *@s : string
+  *Return : void
+  */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
