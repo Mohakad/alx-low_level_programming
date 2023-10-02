e4405005b15b972b9e2a0c102bbd73201cafd38f
Mohakad <mohkadtrd@gmail.com>
2023-10-02 10:59:05 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+*main -  prints its name
+*@argc : command no
+*@argv : command list
+*Return: 0 (success).
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(*(argv[i])))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
