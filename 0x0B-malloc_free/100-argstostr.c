e4292d7e0fa37adc7e32125a826f79ae510e842c
Mohakad <mohkadtrd@gmail.com>
2023-10-04 02:14:43 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,19 +8,30 @@
   */
 char *argstostr(int ac, char **av)
 {
-	char *con;
+	int i, j, r = 0, len = 0;
 
-	int i;
+	char *ar;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	con = malloc(sizeof(av) * ac);
-
-	if (con == NULL)
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+	len += ac;
+	ar = malloc(sizeof(char) * len + 1);
+	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		con[i] = *av[i];
+		for (j = 0; av[i][j]; j++)
+		{
+			ar[r] = av[i][j];
+			r++;
+		}
+		if (ar[r] == '\0')
+			ar[r++] = '\n';
 	}
-	return (con);
+	return (ar);
 }
