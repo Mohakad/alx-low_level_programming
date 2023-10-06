6c71cb74bb2ee2f8c12ef40a5b1ee736c9f72b4d
Mohakad <mohkadtrd@gmail.com>
2023-10-06 07:08:36 -0700

    ./2-calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *_calloc- allocate memory
+  *@nmemb: byte size
+  *@size: allocation size
+  *Return: NULL or arr
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= size; i++)
+	ptr[i] = 0;
+	return (ptr);
+}
