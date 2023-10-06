eb70bbd0fa46f28187242d68990a4f06f4da13fd
Mohakad <mohkadtrd@gmail.com>
2023-10-06 08:24:21 -0700

    ./100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size > old_size)
 		ns = old_size;
-	else if (new_size == old_size)
+	if (new_size == old_size)
 		return (ptr);
 	else if (ptr == NULL)
 	{
