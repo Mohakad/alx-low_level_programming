2e90dfc860f09ab22ae091bf33ea59895c38359e
Mohakad <mohkadtrd@gmail.com>
2023-10-09 10:17:17 -0700

    4newdog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,34 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+  *stcopy- copy
+  *@des: destination
+  *@src: source
+  *Return: copyed string
+  */
+char *stcopy(char *des, char *src)
+{
+	int i;
+
+	for (i=0; src[i] != '\0'; i++)
+		des[i] = src[i];
+	des[i] = '\0';
+	return (des);
+}
+/**
+  *stlen- length
+  *@s: string
+  *Return: length
+  */
+char stlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
   *new_dog- new dot_t
   *@name: new name
   *@age: new age
@@ -11,24 +39,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
 
+	int name_l = stlen(name);
+
+	int own_len = stlen(owner);
+
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = malloc(sizeof(name));
+	newdog->name = malloc(sizeof(char) * (name_l + 1));
 	if (newdog->name == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
-	newdog->owner = malloc(sizeof(owner));
+	newdog->owner = malloc(sizeof(char) * (own_len + 1));
 	if (newdog->owner == NULL)
 	{
 		free(newdog->name);
 		free(newdog);
 		return (NULL);
 	}
-	newdog->name = name;
-	newdog->owner = owner;
+	stcopy(newdog->name, name);
+	stcopy(newdog->owner, owner);
 	newdog->age = age;
 	return (newdog);
 }
