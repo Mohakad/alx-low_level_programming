d61ca0b2ddd393f6e455efbc5a9f594055542b5d
Mohakad <mohkadtrd@gmail.com>
2023-10-09 08:09:21 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,6 +23,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newdog->owner = malloc(sizeof(owner));
 	if (newdog->owner == NULL)
 	{
+		free(newdog->name);
 		free(newdog);
 		return (NULL);
 	}
