0a15cea8a47c3e6cd466f9a0eaea57a81eaa3010
Mohakad <mohkadtrd@gmail.com>
2023-10-09 06:40:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOGST
+#define DOGST
+/**
+  *struct dog- dog information
+  *@name: of dog
+  *@age: of dog
+  *@owner: of dog
+  *Description: full dog info
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
