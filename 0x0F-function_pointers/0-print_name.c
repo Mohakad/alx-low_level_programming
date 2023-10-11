b46828274f39d076e55ab01cb48d461d12fab635
Mohakad <mohkadtrd@gmail.com>
2023-10-11 03:24:23 -0700

    0printname

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+  *print_name- print
+  *@name: name
+  *@f: function
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
