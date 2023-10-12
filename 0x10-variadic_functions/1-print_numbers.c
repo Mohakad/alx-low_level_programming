1c1612218f3d3ba5fe1e3eb8a7d7bd02268aed49
Mohakad <mohkadtrd@gmail.com>
2023-10-12 07:22:32 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,13 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, rs = 0;
 
 	va_list ap;
-	
+
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		rs = va_arg (ap, int);
+		rs = va_arg(ap, int);
 		printf("%d", rs);
-		if (separator != NULL && i != n -1)
+		if (separator != NULL && i != n - 1)
 		printf("%s", separator);
 	}
 	va_end(ap);
