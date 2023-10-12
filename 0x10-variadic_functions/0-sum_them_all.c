abd09526817eacf6587180dd95410aadea6b407d
Mohakad <mohkadtrd@gmail.com>
2023-10-12 07:46:22 -0700

    0-sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdlib.h>
 /**
   *sum_them_all- sum n times
   *@n: number of arguments
@@ -13,9 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(ap, int);
-	}
 	va_end(ap);
 	return (sum);
 }
