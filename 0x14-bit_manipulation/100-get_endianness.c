dcb22b37bb3e589f375ea3d6fcc279c9bcd46075
Mohakad <mohkadtrd@gmail.com>
2023-10-26 20:30:34 +0300

    Little Endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_endianness- checks the endianness.
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int a = 1;
+
+	char *str = (char *)&a;
+
+	if (*str)
+		return (1);
+	else
+		return (0);
+}
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAINH
 #define MAINH
 #include <stdio.h>
+int get_endianness(void);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
