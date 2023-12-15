2afc2c8fe1b70f267c31e7b7af5f4c4867fd2dc4
Mohakad <mohkadtrd@gmail.com>
2023-12-15 01:20:08 -0800

    winnn

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=/$PWD/libwin.so
+wget -O /tmp/libwin.so https://github.com/Mohakad/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so
