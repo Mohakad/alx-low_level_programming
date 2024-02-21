4c1c1b3b6462c1196aaf2531888004af267fe893
Mohakad <mohkadtrd@gmail.com>
2024-02-21 08:17:28 -0800

    island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+
+    Args:
+        grid: is a list of list of integers
+    """
+
