3bf2feca295a5b0df9e03a790caa0113577ceaaf
Mohakad <mohkadtrd@gmail.com>
2024-04-08 22:27:40 +0300

    l

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+/**
+ *  binary_search- searches for a value  using the binary algorithm
+ * @array: pointer to the first element
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index of value
+*/
+int binary_search(int *array, size_t size, int value)
+{
+    size_t i;
+}
